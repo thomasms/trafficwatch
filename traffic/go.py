ad2790a594b176d2e4a9cf45ad8b48760cfae33b
Stainer, Thomas <thomas.stainer@ukaea.uk>
2018-02-07 21:51:48 +0000

    Hack together a script that iterates through all times of the day, in both directions.

--- traffic/go.py
+++ traffic/go.py
@@ -2,21 +2,21 @@ import requests
 import json
 import datetime
 import pytz
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 from traffic._apikey import API_KEY
 
 base_api = "https://maps.googleapis.com/maps/api/directions"
 
-# Lets go from the Savoy in London to Westminster Cathedral
-# at rush hour (08:30) on a Monday
 origin_address = "Savoy+Ct,+London+WC2R+0EZ"
 destination_address = "Francis+St,+Westminster,+London+SW1P+1QW"
-depature_datetime = datetime.datetime(2018, 1, 29, 8, 30, tzinfo=pytz.timezone('Europe/London'))
 
 traffic_model = "pessimistic"
 
 # wraps the api call given a datetimet
-def get_optimal_route_time(dt):
+def get_optimal_route_time(dt, reverse=False):
     # google API depature time works from seconds since midnight 01/01/1970 UTC time
     depature_time_secs = int((dt-datetime.datetime(1970, 1, 1, 0, 0, tzinfo=pytz.timezone('UTC'))).total_seconds())
     departure_time = "{0}".format(depature_time_secs)
@@ -28,7 +28,14 @@ def get_optimal_route_time(dt):
                                                                                                         traffic_model,
                                                                                                         API_KEY
                                                                                                         )
-
+    if reverse:
+        api_url = "{0}/json?origin={1}&destination={2}&departure_time={3}&traffic_model={4}&key={5}".format(base_api,
+                                                                                                            destination_address,
+                                                                                                            origin_address,
+                                                                                                            departure_time,
+                                                                                                            traffic_model,
+                                                                                                            API_KEY
+                                                                                                            )
     r = requests.get(api_url)
     data = r.json()
 
@@ -44,8 +51,34 @@ def get_optimal_route_time(dt):
     return time_in_secs
 
 # Get the time
-t = get_optimal_route_time(depature_datetime)
-print("From: {0}".format(origin_address.replace('+', ' ')))
-print("To: {0}".format(destination_address.replace('+', ' ')))
-print("At: {0}:{1}".format(depature_datetime.hour, depature_datetime.minute))
-print("Quickest route time is: {0} minutes.".format(t/60.))
+hours = np.arange(0, 24, 1)
+mins = np.arange(0, 60, 10)
+
+dt = []
+times = []
+timesrev = []
+for h in hours:
+    for m in mins:
+        dt.append(datetime.datetime(2018, 2, 8, h, m, tzinfo=pytz.timezone('Europe/London')))
+        times.append(get_optimal_route_time(dt[-1], reverse=False)/60.)
+        timesrev.append(get_optimal_route_time(dt[-1], reverse=True)/60.)
+        print(dt[-1], times[-1])
+
+fig, ax = plt.subplots(1)
+ax.plot(dt, times, label='home to work', color='black')
+ax.plot(dt, timesrev, label='work to home', color='red')
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+ax.set_title('My journey to work')
+ax.legend(loc='upper left')
+ax.set_xlabel('leave time (HH:MM)')
+ax.set_ylabel('travel time (minutes)')
+plt.gcf().autofmt_xdate()
+plt.savefig('traffic.eps', format='eps', dpi=1000)
+plt.show()
+
+#depature_datetime = datetime.datetime.now()
+#t = get_optimal_route_time(depature_datetime)
+#print("From: {0}".format(origin_address.replace('+', ' ')))
+#print("To: {0}".format(destination_address.replace('+', ' ')))
+#print("At: {0}:{1}".format(depature_datetime.hour, depature_datetime.minute))
+#print("Quickest route time is: {0} minutes.".format(t/60.))
